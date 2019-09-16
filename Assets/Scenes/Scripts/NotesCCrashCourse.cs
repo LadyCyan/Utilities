dee7ae6a638130c403c853677cca06efbeef7c94
Taylor Renner <taylorrenner@u.boisestate.edu>
2019-09-15 22:12:58 -0600

    Initial commit

---
+++ Assets/Scenes/Scripts/NotesCCrashCourse.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesCCrashCourse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {//this was done in rexster.com coding window, so that is why it doesn't make any sense!!!
+
+        //namespace Rextester
+        //{
+        //  public class Program
+        //{
+        //  public static void Main(string[] args)
+        //{
+        //Your code goes here
+        //  Console.WriteLine("Hello, world!");
+        //
+        //
+        //Point2D myPoint = new Point2D();
+        //Point2D anotherPoint = new Point2D();
+        //anotherPoint.X = 5; 
+        //anotherPoint.Y = 15;
+        //myPoint.AddPoint(anotherPoint);
+        //Console.WriteLine(myPoint.X);
+        //Console.WriteLine(myPoint.Y);
+        
+        
+        //
+        //int meaningOfLife = 42;
+        //float smallPi = 3.14f;
+        //double bigPi = 3.14159265359;
+        //string vaporWare = "Half Life 3";
+        //const bool likesPizza = true;
+        
+        
+        //
+        //
+        //This is a condensed if statement, so instead of writing it like this:
+        
+        //if(likesPizza==false){
+        //console.WriteLine("You monster!");
+        //}
+
+        //Write it like this! All you do is create another variable and then make it equal to whatever value you want it to be.
+        //bool isMonster = (likesPizza == true) ? false : true;
+        //}}}
+        
+        //The structure now deﬁnes a 2D point that contains two properties,
+        //an X and a Y location. 
+        //public indicates that the properties can be changed from outside the object.
+        
+        //If you only wanted to be able to change the properties from within the object
+        //itself, you would mark them as private
+        
+        //To define a structure:
+        
+        //struct Point2D{
+        //public int X;
+        //public int Y;
+        //myPoint.X = 10;
+        //myPoint.Y = 20;
+        
+        //public void AddPoint(Point2D anotherPoint){
+        //this.X = this.X + anotherPoint.X;
+        //this.Y = this.Y + anotherPoint.Y;
+        //}
+        
+        //The big difference between a structure and a class is how they are handled in memory. 
+        //A structure is a value type, whereas a class is a reference type.
+        //A value type exists in the stack, whereas a reference type exists in the heap.
+        //}
+        //}
+
+    }
+}
---
+++ Assets/Scenes/Scripts/Utilities.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class Utilities
+{
+    /*private int NumberWords(string str)
+    {
+      
+
+    private void calculateButton_Click(object sender, EventArgs e)
+    {
+        string allWords = inputBox.Text;
+        if (NumberWords(allWords) > 1)
+        {
+            MessageBox.Show("Words counted.");
+
+        }
+        else
+        {
+            MessageBox.Show("Please input more than one word.");
+        }
+    }*/
+    public static string ProcessText(string input)
+        {
+            int words = 1;
+            int letters = 0;
+            double average;
+
+            foreach (char word in input)
+            {
+                if (char.IsSeparator(word))
+                {
+                    words++;
+
+                }
+                //return words.ToString();
+            }
+            foreach (char letter in input)
+            {
+                if (char.IsLetter(letter) && !char.IsSeparator(letter))
+                {
+                    letters++;
+                }
+            }
+            average = letters / words;
+        return average.ToString();
+        }
+        /*double num;
+        if (double.TryParse(input, out num))
+        {
+            return "Number";
+        }
+        else
+        {
+            return "String";
+        }*/
+        //your code goes here:
+        //determine whether user has input a word or a number.
+        //return the string word if user enters a word (ab.23cd)
+        //return a string 'number' if user enters a number (includes decimal)
+
+
+    }
